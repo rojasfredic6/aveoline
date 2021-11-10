bdd52a27d13cf24520a445cc2dfa2ffb60b143e1
Fredic Yohany Rojas Parra <rojas.fredic6@gmail.com>
2021-11-10 15:38:21 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.container.max-width.grid.h-screen.max-h-full
+  .contenedor.bg-dracula-orange
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style>
+.contenedor{
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+p main
+</template>
+
+<script>
+export default {
+  name: "main"
+}
+</script>
